4994711b535daea0dfee8b287c65817c8032e804
smatsui <smatsui@ubuntu.ubuntu-domain>
2021-09-29 15:30:57 +0900

    Created EFI file with a binary editor

--- src/run_qemu.sh
+++ src/run_qemu.sh
@@ -5,7 +5,7 @@ mkfs.fat -n 'MYMIKAN OS' -s 2 -f 2 -R 32 -F 32 disk.img
 mkdir -p mnt
 sudo mount -o loop disk.img mnt
 sudo mkdir -p mnt/EFI/BOOT
-sudo cp BOOTX64.EFI mnt/EFI/BOOT/BOOTX64.EFI
+sudo cp hello.efi mnt/EFI/BOOT/BOOTX64.EFI
 sudo umount mnt
 qemu-system-x86_64 \
     -drive if=pflash,file=$HOME/osbook/devenv/OVMF_CODE.fd \
