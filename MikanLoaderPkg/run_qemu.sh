64c179e761ef948a6c57f9c46a21af8cd25e634f
smatsui <sosuke.matsui@gmail.com>
2021-09-30 17:26:05 +0900

    Added initial kernel code

--- MikanLoaderPkg/run_qemu.sh
+++ MikanLoaderPkg/run_qemu.sh
@@ -10,4 +10,5 @@ sudo umount mnt
 qemu-system-x86_64 \
     -drive if=pflash,file=$HOME/osbook/devenv/OVMF_CODE.fd \
     -drive if=pflash,file=$HOME/osbook/devenv/OVMF_VARS.fd \
+    -monitor stdio \
     -hda disk.img
